d21d736fc6410bf598d569dda411fefef79a9762
Cyril <cyril@collen.ch>
2023-03-20 20:38:24 +0100

    init project

---
+++ src/test/java/com/example/modulith/demomodulith/DemoModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulith.demomodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
